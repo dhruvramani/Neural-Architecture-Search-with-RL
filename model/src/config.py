eca19cb514a4b2739d02ece7910be645f4cf10e6
dhruvramani <dhruvramani98@gmail.com>
2017-07-23 22:16:40 +0530

    foo

---
+++ model/src/config.py
@@ -0,0 +1,35 @@
+import os
+import utils
+import tensorflow as tf
+
+class Config(object):
+    def __init__(self, args):
+        self.codebase_root_path = args.path
+        self.folder_suffix = args.folder_suffix
+        self.project_name = args.project
+        self.dataset_name = args.dataset
+        self.batch_size = args.batch_size
+        self.max_epochs = args.max_epochs
+
+        class Solver(object):
+            def __init__(self, t_args):
+                self.learning_rate = t_args.lr
+                self.dropout = t_args.dropout
+                if t_args.opt.lower() not in ["adam", "rmsprop", "sgd"]: 
+                    raise ValueError('Undefined type of optmizer')
+                else:  
+                    self.optimizer = {"adam": tf.train.AdamOptimizer, "rmsprop": tf.train.RMSPropOptimizer, "sgd": tf.train.GradientDescentOptimizer}[t_args.opt.lower()]
+        
+        self.solver = Solver(args)
+        self.project_path, self.project_prefix_path, self.dataset_path, self.train_path, self.test_path, self.ckptdir_path = self.set_paths()
+        
+
+    def set_paths(self):
+        project_path = utils.path_exists(self.codebase_root_path)
+        project_prefix_path = "" #utils.path_exists(os.path.join(self.codebase_root_path, self.project_name, self.folder_suffix))
+        dataset_path = utils.path_exists(os.path.join(self.codebase_root_path, "../data", self.dataset_name))
+        ckptdir_path = utils.path_exists(os.path.join(self.codebase_root_path, "bin"))
+        train_path = os.path.join(dataset_path, self.dataset_name + "-train")
+        test_path = os.path.join(dataset_path, self.dataset_name + "-test")
+
+        return project_path, project_prefix_path, dataset_path, train_path, test_path, ckptdir_path
---
+++ model/src/parser.py
@@ -0,0 +1,28 @@
+import argparse
+
+class Parser(object):
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--path", default="../", help="Base Path for the Folder")
+        parser.add_argument("--project", default="NASuRL", help="Project Folder")
+        parser.add_argument("--folder_suffix", default="Default", help="Folder Name Suffix")
+        parser.add_argument("--dataset", default="cifar-10", help="Name of the Dataset")
+        parser.add_argument("--opt", default="adam", help="Optimizer : adam, rmsprop, sgd")
+        parser.add_argument("--lr", default=0.001, help="Learning Rate", type=float)
+        parser.add_argument("--batch_size", default=50, help="Batch Size", type=int)
+        parser.add_argument("--dropout", default=0.5, help="Dropout Probab. for Pre-Final Layer", type=float)
+        parser.add_argument("--max_epochs", default=100, help="Maximum Number of Epochs", type=int)
+        parser.add_argument("--debug", default=False, type=self.str_to_bool, help="Debug Mode")
+        parser.add_argument("--load", default =False, type=self.str_to_bool, help="Load Model to calculate accuracy")
+        self.parser=parser
+
+    def str_to_bool(self, string):
+        if string.lower() == "true":
+            return True
+        elif string.lower() == "false":
+            return False
+        else :
+            return argparse.ArgumentTypeError("Boolean Value Expected")
+
+    def get_parser(self):
+        return self.parser
