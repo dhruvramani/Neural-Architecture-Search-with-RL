647a29b0e8e3e03405159085c0e9ee51249c34b7
dhruvramani <dhruvramani98@gmail.com>
2017-09-18 19:05:32 +0530

    LeNet Added

--- model_lenet/src/network.py
+++ model_lenet/src/network.py
@@ -12,7 +12,8 @@ class Network(object):
         self.state = tf.Variable(tf.zeros(shape=[1, 4]))
         self.lstm = tf.contrib.rnn.BasicLSTMCell(self.n_hidden, forget_bias=1.0, state_is_tuple=False)
         self.Wc, self.bc = self.init_controller_vars()
-        self.Wconv, self.bconv, self.Wf1, self.bf1, self.Wf2, self.bf2 = None, None, None, None, None, None
+        self.Wconv1, self.bconv1, self.Wconv2, self.bconv2, self.Wconv3, self.bconv3 =  None, None, None, None, None, None
+        self.Wf1, self.bf1, self.Wf2, self.bf2 = None, None, None, None
     
     def weight_variable(self, shape, name):
         return tf.get_variable(name=name, shape=shape, initializer=tf.contrib.layers.xavier_initializer())
@@ -32,7 +33,7 @@ class Network(object):
             inp, self.state = self.lstm(inp, self.state)
             inp = tf.nn.softmax(tf.matmul(inp, self.Wc) + self.bc)
             output.append(inp[0, :])
-        out = [utils.max(output[i]) for i in range(6)]
+        out = [utils.max(output[i]) for i in range(self.n_steps)]
         return out, output[-1]
 
     def gen_hyperparams(self, output):
@@ -40,27 +41,49 @@ class Network(object):
         n_filters = tf.constant([24, 36, 48, 64], dtype=tf.int32)
         strides = tf.constant([1, 2, 3, 4], dtype=tf.int32)
         hyperparams = [1, 1, 1, 1, 1, 1]
+        # Change the following based on number of hyperparameters to be predicted
+        # Removing strides for now
         hyperparams[0], hyperparams[1] = filter_dims[output[0]], filter_dims[output[1]]
-        hyperparams[2], hyperparams[3] = strides[output[2]], strides[output[3]]
-        hyperparams[4], hyperparams[5] = n_filters[output[4]], n_filters[output[5]]
+        hyperparams[2] = n_filters[output[2]]  # Layer 1
+        hyperparams[3], hyperparams[4] = filter_dims[output[3]], filter_dims[output[5]]
+        hyperparams[5] = n_filters[output[5]]  # Layer 2
+        hyperparams[6], hyperparams[7] = filter_dims[output[6]], filter_dims[output[7]]
+        hyperparams[8] = n_filters[output[8]] # Layer 3
+        hyperparams[9] = n_filters[output[9]] # FNN Layer
+
         return hyperparams
 
     def construct_model(self, data, hyperparams, keep_prob):
         data = tf.expand_dims(data, -1)
         data = tf.expand_dims(data, -1)
-        hyperparams = {"filter_row": hyperparams[0], "filter_column": hyperparams[1], "stride_height": hyperparams[2], "stride_width": hyperparams[3], "n_filter": hyperparams[4], "n_autoneurons": hyperparams[5]}
+        hyperparams = {"filter_row_1": hyperparams[0], "filter_column_1": hyperparams[1], "n_filter_1": hyperparams[2], "filter_row_2": hyperparams[3], "filter_column_2": hyperparams[4], "n_filter_2": hyperparams[5], "filter_row_3": hyperparams[6], "filter_column_3": hyperparams[7], "n_filter_3": hyperparams[8], "n_autoneurons": hyperparams[9]}
         images = tf.reshape(data, [self.config.batch_size, 32, 32, 3])
-        self.Wconv = self.weight_variable(shape=[hyperparams["filter_row"], hyperparams["filter_column"], 3, hyperparams["n_filter"]], name="kernel")
-        self.bconv = self.bias_variable(shape=[hyperparams["n_filter"]], name="b_conv")
+        self.Wconv1 = self.weight_variable(shape=[hyperparams["filter_row_1"], hyperparams["filter_column_1"], 3, hyperparams["n_filter_1"]], name="kernel_1")
+        self.bconv1 = self.bias_variable(shape=[hyperparams["n_filter_1"]], name="b_conv_1")
+        self.Wconv2 = self.weight_variable(shape=[hyperparams["filter_row_2"], hyperparams["filter_column_2"], hyperparams["n_filter_1"], hyperparams["n_filter_2"]], name="kernel_2")
+        self.bconv2 = self.bias_variable(shape=[hyperparams["n_filter_2"]], name="b_conv_2")
+        self.Wconv3 = self.weight_variable(shape=[hyperparams["filter_row_3"], hyperparams["filter_column_3"], hyperparams["n_filter_2"], hyperparams["n_filter_3"]], name="kernel_3")
+        self.bconv3 = self.bias_variable(shape=[hyperparams["n_filter_3"]], name="b_conv_3")
         self.bf1 = self.bias_variable(shape=[hyperparams["n_autoneurons"]], name="b_fc1")
         self.Wf2 = self.weight_variable(shape=[hyperparams["n_autoneurons"], 10], name="w_fc2")
         self.bf2 = self.bias_variable(shape=[10], name="b_fc2")
-        convlove = tf.nn.conv2d(images, self.Wconv, strides=[1, hyperparams["stride_height"], hyperparams["stride_width"], 1], padding="SAME")
-        convlove = tf.nn.bias_add(convlove, self.bconv)
-        shape = convlove.get_shape().as_list()
-        mult = shape[1] * shape[2] * shape[3] 
+
+        conv1 = tf.nn.conv2d(images, self.Wconv1, strides=[1, 1, 1, 1], padding="SAME")
+        conv1 = tf.nn.bias_add(conv1, self.bconv1)
+        pool1 = tf.nn.max_pool(conv1, [1, 5, 5, 1], [1, 1, 1, 1], padding = 'VALID')        
+        conv2 = tf.nn.conv2d(pool1, self.Wconv2, strides=[1, 1, 1, 1], padding="SAME")
+        conv2 = tf.nn.bias_add(conv2, self.bconv2)
+        pool2 = tf.nn.max_pool(conv2, [1, 5, 5, 1], [1, 1, 1, 1], padding = 'VALID')
+        conv3 = tf.nn.conv2d(pool2, self.Wconv3, strides=[1, 1, 1, 1], padding="SAME")
+        conv3 = tf.nn.bias_add(conv3, self.bconv3)
+        pool3 = tf.nn.max_pool(conv3, [1, 5, 5, 1], [1, 1, 1, 1], padding = 'VALID')
+
+        shape = pool3.get_shape().as_list()
+        mult = 1
+        for i in shape:
+            mult *= i
         self.Wf1 = self.weight_variable(shape=[mult, hyperparams["n_autoneurons"]], name="w_fc1")
-        reshaped = tf.reshape(convlove, [shape[0], mult])
+        reshaped = tf.reshape(pool3, [shape[0], mult])
         fc = tf.nn.dropout(utils.leaky_relu(tf.matmul(reshaped, self.Wf1) + self.bf1), keep_prob)
         output = tf.matmul(fc, self.Wf2)+ self.bf2
         return output
@@ -70,7 +93,7 @@ class Network(object):
 
     def train_model(self, loss):
         optimizer = self.config.solver.optimizer
-        var_list = [self.Wconv, self.bconv, self.Wf1, self.bf1, self.Wf2, self.bf2]
+        var_list = [self.Wconv1, self.bconv1, self.Wconv2, self.bconv2, self.Wconv3, self.bconv3]
         return optimizer.minimize(loss, var_list=var_list)
 
     def accuracy(self, logits, labels):
