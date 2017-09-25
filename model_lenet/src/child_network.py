595dd7fd8ec38a06d708a339f1620a0687b94300
dhruvramani <dhruvramani98@gmail.com>
2017-09-25 19:28:35 +0530

    Hello World

--- model_lenet/src/child_network.py
+++ model_lenet/src/child_network.py
@@ -9,7 +9,8 @@ class ChildNetwork(object):
         self.config = config
         self.hyperparams = {"filter_row_1": hyperparams[0], "filter_column_1": hyperparams[1], "n_filter_1": hyperparams[2], "filter_row_2": hyperparams[3], "filter_column_2": hyperparams[4], "n_filter_2": hyperparams[5], "filter_row_3": hyperparams[6], "filter_column_3": hyperparams[7], "n_filter_3": hyperparams[8], "n_autoneurons": hyperparams[9]}
         self.Wconv1, self.bconv1, self.Wconv2, self.bconv2, self.Wconv3, self.bconv3 = self.init_conv_vars()
-        self.Wf1, self.bf1, self.Wf2, self.bf2, self.Wf3, self.bf3, self.Wf4, self.bf4 = self.init_fc_vars()
+        '''self.Wf1, self.bf1, self.Wf2, self.bf2,''' 
+        self.Wf3, self.bf3, self.Wf4, self.bf4 = self.init_fc_vars()
 
     def weight_variable(self, shape, name):
         return tf.Variable(tf.random_normal(shape=shape), name=name)
@@ -30,15 +31,17 @@ class ChildNetwork(object):
         data = np.zeros((self.config.batch_size, 32*32*3), dtype=np.float32)
         keep_prob = 0.5
         dim, _ =  self.run_cnn(data, keep_prob)
+        '''
         Wf1 = self.weight_variable(shape=[dim, 384], name="w_fc1")
         bf1 = self.bias_variable(shape=[384], name="b_fc1")
         Wf2 = self.weight_variable(shape=[384, 192], name="w_fc2")
         bf2 = self.bias_variable(shape=[192], name="b_fc2")
+        '''
         Wf3 = self.weight_variable(shape=[dim, self.hyperparams["n_autoneurons"]], name="w_fc3")
         bf3 = self.bias_variable(shape=[self.hyperparams["n_autoneurons"]], name="b_fc3")
         Wf4 = self.weight_variable(shape=[self.hyperparams["n_autoneurons"], self.config.num_classes], name="w_fc4")
         bf4 = self.bias_variable(shape=[self.config.num_classes], name="b_fc4")
-        return Wf1, bf1, Wf2, bf2, Wf3, bf3, Wf4, bf4
+        return Wf3, bf3, Wf4, bf4 #Wf1, bf1, Wf2, bf2,
 
     def run_cnn(self, data, keep_prob):
         data = tf.expand_dims(data, -1)
@@ -69,13 +72,15 @@ class ChildNetwork(object):
         return output
 
     def model_loss(self, logits, labels):
-        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+        softmax = tf.exp(logits) / tf.reduce_sum(tf.exp(logits))
+        return -tf.reduce_mean(labels * tf.log(1e-10 + softmax))
+        #tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
 
     def train_model(self, loss):
         optimizer = self.config.solver.optimizer
-        var_list = [self.Wconv1, self.bconv1, self.Wconv2, self.bconv2, self.Wconv3, self.bconv3, self.Wf1, self.bf1, self.Wf2, self.bf2, self.Wf3, self.bf3, self.Wf4, self.bf4]
+        '''self.Wf1, self.bf1, self.Wf2, self.bf2,'''
+        var_list = [self.Wconv1, self.bconv1, self.Wconv2, self.bconv2, self.Wconv3, self.bconv3, self.Wf3, self.bf3, self.Wf4, self.bf4]
         return optimizer.minimize(loss, var_list=var_list)
 
     def accuracy(self, logits, labels):
         return 100.0 * tf.reduce_mean(tf.cast(tf.equal(utils.max(logits), utils.max(labels)), tf.float32))
-
